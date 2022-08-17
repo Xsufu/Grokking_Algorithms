f206253300d7e3bbb72c490355cd65f9eaa8be12
Xsufu <aluno.macarrao@gmail.com>
2022-08-17 20:54:29 +0700

    Тесты быстрой сортировки

--- src/test/kotlin/SortingTest.kt
+++ src/test/kotlin/SortingTest.kt
@@ -12,4 +12,14 @@ internal class SortingTest {
         assertEquals(listOf(1, 2, 3, 4, 5, 6), testClass.selectionSort(mutableListOf(6, 3, 2, 1, 4, 5)))
         assertEquals(listOf(1, 2, 3, 3, 4), testClass.selectionSort(mutableListOf(3, 1, 3, 2, 4)))
     }
+
+    @Test
+    fun quickSort() {
+        assertEquals(listOf(1, 2, 3, 4, 5, 6), testClass.quickSort(mutableListOf(6, 3, 2, 1, 4, 5)))
+        assertEquals(listOf(1, 2, 3, 3, 4), testClass.quickSort(mutableListOf(3, 1, 3, 2, 4)))
+        assertEquals(listOf(1, 2, 2, 3, 4), testClass.quickSort(mutableListOf(3, 2, 1, 4, 2)))
+        assertEquals(listOf(1, 2, 3, 4, 5), testClass.quickSort(mutableListOf(1, 2, 3, 4, 5)))
+        assertEquals(listOf(1, 1, 1, 1), testClass.quickSort(mutableListOf(1, 1, 1, 1)))
+        assertEquals(listOf(1, 1, 2, 3, 4), testClass.quickSort(mutableListOf(1, 4, 2, 3, 1)))
+    }
 }

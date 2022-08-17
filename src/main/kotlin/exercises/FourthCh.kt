b13eff3d8ba812af012ee3ae8d6e751ace279cf1
Xsufu <aluno.macarrao@gmail.com>
2022-08-17 20:03:59 +0700

    Поиск наибольшего числа в списке через рекурсию

--- src/main/kotlin/exercises/FourthCh.kt
+++ src/main/kotlin/exercises/FourthCh.kt
@@ -3,6 +3,7 @@ package exercises
 fun main() {
     println(sum(listOf(2, 3, 4, 5)))
     println(countOfElements(listOf(2, 3, 4, 5)))
+    println(findBiggest(listOf(7, 3, 6, 5)))
 }
 
 fun sum(list: List<Int>): Int {
@@ -17,4 +18,18 @@ fun countOfElements(list: List<Int>): Int {
         0
     else
         1 + countOfElements(list.slice(1..list.lastIndex))
+}
+
+fun findBiggest(list: List<Int>): Int {
+    if (list.size == 2) {
+        return if (list[0] > list[1])
+            list[0]
+        else
+            list[1]
+    }
+    val tempMax = findBiggest(list.slice(1..list.lastIndex))
+    return if (list[0] > tempMax)
+        list[0]
+    else
+        tempMax
 }

55074e978725dc932b7b02d657a51249e97a1d9d
Xsufu <aluno.macarrao@gmail.com>
2022-08-17 19:56:53 +0700

    Алгоритм подсчёта количества элементов в списке через рекурсию

--- src/main/kotlin/exercises/FourthCh.kt
+++ src/main/kotlin/exercises/FourthCh.kt
@@ -2,6 +2,7 @@ package exercises
 
 fun main() {
     println(sum(listOf(2, 3, 4, 5)))
+    println(countOfElements(listOf(2, 3, 4, 5)))
 }
 
 fun sum(list: List<Int>): Int {
@@ -9,4 +10,11 @@ fun sum(list: List<Int>): Int {
         0
     else
         list[0] + sum(list.slice(1..list.lastIndex))
+}
+
+fun countOfElements(list: List<Int>): Int {
+    return if (list.isEmpty())
+        0
+    else
+        1 + countOfElements(list.slice(1..list.lastIndex))
 }
